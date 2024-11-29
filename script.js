3d3dba673f70aa2934075d6820672c340854b37e
MesratiNour <mesratinour@gmail.com>
2024-11-29 15:45:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// Défilement fluide vers les sections
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault(); // Empêche le comportement par défaut du lien
-        const targetId = this.getAttribute('href').substring(1); // Récupère l'id de la section
-        const targetSection = document.getElementById(targetId);
-        
-        if (targetSection) {
-            window.scrollTo({
-                top: targetSection.offsetTop - 50, // Ajuste la position pour éviter un chevauchement avec la navigation
-                behavior: 'smooth'
-            });
-        }
-    });
-});
+
 
 // Changement dynamique de couleur dans le menu de navigation
 const navLinks = document.querySelectorAll('nav ul li a');
